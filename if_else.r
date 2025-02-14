b13cf2b8c3156bad9fb9708b20f3143bd815eca1
Frozepunk <smg2750935@gmail.com>
2025-02-14 09:07:09 +0530

    just writing some code in classs

---
+++ if_else.r
@@ -0,0 +1,11 @@
+x <- 30L  
+if(is.integer(x)){  
+print("X is an Integer")  
+} 
+
+y <- c("what","is","truth")  
+if("Truth" %in% y){  
+print("Truth is found")  
+} else {  
+print("Truth is not found")  
+}
---
+++ variables.r
@@ -0,0 +1,7 @@
+# Assignment using equal operator.  
+var.1 = c(0,1,2,3)             
+# Assignment using leftward operator.  
+var.2 <- c("learn","R")     
+# Assignment using rightward operator.     
+c(TRUE,1) -> var.3             
+print(var.1) 
